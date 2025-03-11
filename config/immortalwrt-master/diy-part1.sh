0e75cc6c38750e6517d1260ae18facc11e4c5ac7
Nightdog <60809507+H2295@users.noreply.github.com>
2025-03-11 16:45:36 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -9,6 +9,7 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main'  feeds.conf.default
+sed -i 's/$src-git .*\.git$/\1;openwrt-24.10/g' feeds.conf.default
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
